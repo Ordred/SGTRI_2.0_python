46fca720f22ca247ab437b39e92be89c0c503103
Ordred <jonathan.albrecht@students.hevs.ch>
2022-03-16 10:42:58 +0100

    fixed none id in motifquestion

--- motifquestion.py
+++ motifquestion.py
@@ -17,6 +17,4 @@ class MotifQuestion(BaseModel):
 
     def to_bson(self):
         data = self.dict(by_alias=True, exclude_none=True)
-        if data["_id"] is None:
-            data.pop("_id")
         return data
