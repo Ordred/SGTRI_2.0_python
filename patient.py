0389a91395fe3204ef48d77cba01cb35fa4f7378
Ordred <jonathan.albrecht@students.hevs.ch>
2022-03-17 08:53:36 +0100

    removed id check for patient

--- patient.py
+++ patient.py
@@ -30,6 +30,4 @@ class Patient(BaseModel):
 
     def to_bson(self):
         data = self.dict(by_alias=True, exclude_none=True)
-        if data["_id"] is None:
-            data.pop("_id")
         return data
